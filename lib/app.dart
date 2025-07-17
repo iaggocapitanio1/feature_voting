7b94810e877fed43038b61c069559a247e0bbf9f
iaggocapitanio1 <iaggo.capitanio@gmail.com>
2025-07-17 08:54:42 -0300

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FeatureVotingApp extends StatefulWidget {
+  const FeatureVotingApp({super.key});
+
+  @override
+  State<FeatureVotingApp> createState() => _FeatureVotingAppState();
+}
+
+class _FeatureVotingAppState extends State<FeatureVotingApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:feature_voting/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FeatureVotingApp());
+}
